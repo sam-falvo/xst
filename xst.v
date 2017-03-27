f43c0bb0796535c1693f309df0af119f93bf6ee6
Samuel A. Falvo II <kc5tja@arrl.net>
2017-03-26 21:40:49 -0700

    TXC output

--- xst.v
+++ xst.v
@@ -12,6 +12,7 @@ module xst(
 	input	[5:0]	bits_i,
 
 	output		txd_o,
+	output		txc_o,
 	output		idle_o,
 	output	[15:0]	brg_o,
 	output	[5:0]	bits_o
@@ -20,38 +21,48 @@ module xst(
 	reg	[5:0]	bits_o;
 	reg	[15:0]	brg_o;
 	reg	[63:0]	shift_register;
+	reg		txc_o;
 
 	assign		txd_o = shift_register[0];
 	assign		idle_o = ~|bits_o;
 
-	wire		txreg_shift = (~|brg_o) & ~idle_o;
+	wire		txreg_shift = ~|brg_o;
+	wire	[15:1]	halfbit = txbaud_i[15:1];
 
 	always @(posedge clk_i) begin
 		shift_register <= shift_register;
 		brg_o <= brg_o;
+		txc_o <= txc_o;
 		bits_o <= bits_o;
 
 		if(reset_i) begin
 			shift_register <= ~(64'd0);
 			brg_o <= 0;
+			txc_o <= 0;
 			bits_o <= 0;
 		end
 
 		else if(txreg_we_i) begin
 			brg_o <= txbaud_i;
+			txc_o <= 1;
 			shift_register <= dat_i;
 			bits_o <= bits_i;
 		end
 
 		else begin
-			if(txreg_shift) begin
+			if(txreg_shift & ~idle_o) begin
 				brg_o <= txbaud_i;
+				txc_o <= (bits_o != 6'd1);
 				shift_register <= {rxd_i, shift_register[63:1]};
 				bits_o <= bits_o - 1;
 			end
 			else if(~txreg_shift & ~idle_o) begin
 				brg_o <= brg_o - 1;
 			end
+		end
+
+		if(brg_o == {1'b0, halfbit}) begin
+			txc_o <= 0;
 		end
 	end
 endmodule
--- xst_tb.v
+++ xst_tb.v
@@ -7,20 +7,22 @@ module xst_tb(
 	reg		txreg_oe_i = 0, txreg_we_i = 0;
 	reg	[63:0]	dat_i;
 
-	wire		txd_o, idle_o;
+	wire		txd_o, txc_o, idle_o;
 	wire	[15:0]	brg_o;
 
 	xst x(
 		.clk_i(clk_i),
 		.reset_i(reset_i),
 
-		.rxd_i(1'b1),
+		.rxd_i(txd_o),
 		.dat_i(dat_i),
 
 		.bits_i(6'd11),
 		.txreg_we_i(txreg_we_i),
+		.txreg_oe_i(1'b1),
 		.txbaud_i(16'h0004),
 		.txd_o(txd_o),
+		.txc_o(txc_o),
 		.idle_o(idle_o),
 		.brg_o(brg_o)
 	);
@@ -49,6 +51,16 @@ module xst_tb(
 	end
 	endtask
 
+	task assert_txc;
+	input expected;
+	begin
+		if (expected !== txc_o) begin
+			$display("@E %d txc_o Expected %d; got %d", story, expected, txc_o);
+			$stop;
+		end
+	end
+	endtask
+
 	task assert_brg;
 	input [15:0] expected;
 	begin
@@ -62,11 +74,11 @@ module xst_tb(
 	task test_bitcell;
 	input expected_txd;
 	begin
-		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(4);
-		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(3);
-		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(2);
-		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(1);
-		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(0);
+		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(4); assert_txc(1);
+		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(3); assert_txc(1);
+		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(2); assert_txc(1);
+		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(1); assert_txc(0);
+		wait(~clk_i); wait(clk_i); #1 assert_txd(expected_txd); assert_idle(0); assert_brg(0); assert_txc(0);
 	end
 	endtask
 
